6f90ee8f842822c218e7f4a8fa6c21191b2743a6
TheDanishMexican <danieljchristjansen@gmail.com>
2023-02-03 11:26:51 +0100

    added button
    added button

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
 document.body.innerHTML += "";
 //alert("You may fall into an addiction, proceed with caution");
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Butter Chicken";
+}
